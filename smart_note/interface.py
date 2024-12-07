61a0daad3ed60c57eabf1b0a91b4227eb6034fd2
Mynekene <Mynekene@gmail.com>
2024-12-07 13:52:03 +0200

    Update interface.py

--- smart_note/interface.py
+++ smart_note/interface.py
@@ -6,7 +6,7 @@ from PyQt5.QtCore import Qt
 
 app = QApplication([])
 window = QWidget()
-window.setFixedSize(900, 700)
+window.setFixedSize(1000, 620)
 window.setWindowTitle("Smart Note")
 
 create_note = QPushButton("Create Note")
@@ -15,20 +15,33 @@ delete_note = QPushButton("Delete Note")
 create_tag = QPushButton("Create Tag")
 delete_tag = QPushButton("Delete Tag")
 search_tag = QPushButton("Search Tag")
-label_note = QLabel("")
-label_tag = QLabel("")
-note_list_widget = QListWidget("")
-tag_list_widget = QListWidget("")
-searching_tag_input = QLineEdit("")
-text_edit = QInputDialog("")
+label_note = QLabel("Note List")
+label_tag = QLabel("Tag List")
+note_list_widget = QListWidget()
+tag_list_widget = QListWidget()
+searching_tag_input = QInputDialog()
+text_edit = QTextEdit()
+text_edit.setDisabled(True)
 
-layout1 
+layout1 = QHBoxLayout()
+layout1.addWidget(create_note)
+layout1.addWidget(save_note)
 
-layout2
+layout2 = QHBoxLayout()
+layout2.addWidget(delete_note)
+layout2.addWidget(create_tag)
 
-panel_layout
+panel_layout = QHBoxLayout()
+panel_layout.addWidget(label_note)
+panel_layout.addWidget(label_tag)
+panel_layout.addWidget(note_list_widget)
+panel_layout.addWidget(tag_list_widget)
+panel_layout.addWidget(searching_tag_input)
+panel_layout.addWidget(text_edit)
+panel_layout.addWidget(delete_tag)
+panel_layout.addWidget(search_tag)
 
-main_layout
+main_layout = QVBoxLayout()
+main_layout.addLayout(panel_layout)
 
 window.show()
-app.exec_()
